4029c25137c9ae1bfc0ef714d031243b41f02747
Uros Petronijevic <ukipetlja@gmail.com>
2025-01-08 17:32:47 +0100

    Simple Navigation was added.

--- src/Components/Navigation Components/Navigation.tsx
+++ src/Components/Navigation Components/Navigation.tsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+
 export default function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <nav className="bg-gray-800 text-white py-4 px-10 flex gap-4 justify-between">
+      <Link to="/table1" className="hover:underline">
+        Mor
+      </Link>
+      <Link to="/table2" className="hover:underline">
+        DodatnoOpterecenje
+      </Link>
+      <Link to="/table3" className="hover:underline">
+        Pripravnost
+      </Link>
+      <Link to="/table4" className="hover:underline">
+        Prekovremeni
+      </Link>
+    </nav>
+  );
 }
